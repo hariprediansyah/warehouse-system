5fbde401cbf8b3115f20f2616b80174dcd548281
Hari Prediansyah <coyhari9@gmail.com>
2024-04-30 09:58:17 +0700

    first

---
+++ Warehouse/Pages/Master.cshtml.cs
@@ -0,0 +1,37 @@
+using System.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Warehouse.Class;
+
+namespace Warehouse.Pages
+{
+	public class MasterModel : PageModel
+	{
+		public void OnGet()
+		{
+
+		}
+
+		public DataTable GetAllCategory(){
+            using SqlHelper sqlHelper = new SqlHelper();
+            sqlHelper.commandText = "SELECT * FROM MsCategory";
+            return sqlHelper.ExecuteDataTable();
+        }
+	}
+
+	public class MasterData
+	{
+		public string Code { get; set; }
+		public string Name { get; set; }
+		public string Description { get; set; }
+		public string UserInput { get; set; }
+
+		public MasterData(string Code, string Name, string Description, string UserInput)
+		{
+			this.Code = Code;
+			this.Name = Name;
+			this.Description = Description;
+			this.UserInput = UserInput;
+		}
+	}
+}
---
+++ Warehouse/class/SqlHelper.cs
@@ -0,0 +1,80 @@
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace Warehouse.Class
+{
+    public class SqlHelper : IDisposable
+    {
+        const string connectionString = "Server=localhost;Database=Tugas;Uid=sa;Pwd=qwertyuiop;";
+        private MySqlConnection connection = null;
+        MySqlCommand cmd = null;
+        public string commandText = "";
+        public CommandType commandType = CommandType.Text;
+        private bool disposedValue;
+
+        public SqlHelper() {
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+            cmd = connection.CreateCommand();
+        }
+
+        public DataTable ExecuteDataTable()
+        {
+            cmd.CommandText = commandText;
+            cmd.CommandType = commandType;
+            MySqlDataAdapter adapter = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            adapter.Fill(dt);
+            return dt;
+        }
+
+        public void ExecuteNonQuery()
+        {
+            cmd.CommandText = commandText;
+            cmd.CommandType = commandType;
+            cmd.ExecuteNonQuery();
+        }
+
+        public void AddParameter(string parameterName, string value, MySqlDbType mySqlDbType)
+        {
+            var param = new MySqlParameter
+            {
+                ParameterName = parameterName,
+                Value = value,
+                MySqlDbType = mySqlDbType
+            };
+            cmd.Parameters.Add(param);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                if (disposing)
+                {
+                    // TODO: dispose managed state (managed objects)
+                    connection.Close();
+                    connection.Dispose();
+                }
+
+                // TODO: free unmanaged resources (unmanaged objects) and override finalizer
+                // TODO: set large fields to null
+                disposedValue = true;
+            }
+        }
+
+        // // TODO: override finalizer only if 'Dispose(bool disposing)' has code to free unmanaged resources
+        // ~MySqlHelper()
+        // {
+        //     // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+        //     Dispose(disposing: false);
+        // }
+
+        public void Dispose()
+        {
+            // Do not change this code. Put cleanup code in 'Dispose(bool disposing)' method
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
